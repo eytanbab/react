aff3bfbedc1bc08c5c01a07daab0e8a8e078de7b
eytanbab <73233191+eytanbab@users.noreply.github.com>
2023-03-07 03:23:07 +0200

    changed state name

--- dictionary-web-app/src/components/Nav.jsx
+++ dictionary-web-app/src/components/Nav.jsx
@@ -3,16 +3,16 @@ import { FaBook } from 'react-icons/fa';
 import { BsToggle2Off, BsToggle2On, BsMoon, BsSun } from 'react-icons/bs';
 
 const Nav = () => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [lightMode, setLightMode] = useState(false);
   return (
     <nav className='flex h-16 w-full items-center justify-between bg-gray-100  px-6'>
       <FaBook size={30} />
       <div className='flex items-center gap-8'>
-        <button onClick={() => setDarkMode(!darkMode)}>
-          {!darkMode ? <BsToggle2Off size={30} /> : <BsToggle2On size={30} />}
+        <button onClick={() => setLightMode(!lightMode)}>
+          {lightMode ? <BsToggle2Off size={30} /> : <BsToggle2On size={30} />}
         </button>
         <div className='flex items-center justify-center '>
-          {!darkMode ? <BsSun size={20} /> : <BsMoon size={20} />}
+          {lightMode ? <BsSun size={20} /> : <BsMoon size={20} />}
         </div>
       </div>
     </nav>
