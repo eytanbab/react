cb9b7b804158e4784a816075e71f83fc716da573
eytanbab <73233191+eytanbab@users.noreply.github.com>
2023-03-08 17:46:54 +0200

    added loading

--- dictionary-web-app/src/App.jsx
+++ dictionary-web-app/src/App.jsx
@@ -3,17 +3,30 @@ import SearchBar from './components/SearchBar';
 import { useState } from 'react';
 import Result from './components/Result';
 import { useEffect } from 'react';
+
 function App() {
   const [word, setWord] = useState('');
   const [definition, setDefinition] = useState([]);
   const [lightMode, setLightMode] = useState(true);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   const getDefinition = async () => {
-    const api = await fetch(
-      'https://api.dictionaryapi.dev/api/v2/entries/en/' + word
-    );
-    const data = await api.json();
-    setDefinition(data);
+    try {
+      if (word !== '') {
+        setIsLoaded(false);
+        const api = await fetch(
+          'https://api.dictionaryapi.dev/api/v2/entries/en/' + word
+        );
+        const data = await api.json();
+        if (data.title !== 'No Definitions Found') {
+          console.log(data);
+          setDefinition(data);
+          setIsLoaded(true);
+        }
+      }
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   useEffect(() => {
@@ -22,13 +35,12 @@ function App() {
 
   return (
     <div
-      className={`flex h-auto min-h-screen flex-col items-center gap-8 ${
-        lightMode ? 'bg-gray-200' : 'bg-gray-700'
-      }`}
+      className={`flex h-auto min-h-screen flex-col items-center gap-8 
+      ${lightMode ? 'bg-gray-200' : 'bg-gray-700'}`}
     >
       <Nav lightMode={lightMode} setLightMode={setLightMode} />
       <SearchBar setWord={setWord} />
-      {word && <Result definition={definition} lightMode={lightMode} />}
+      {isLoaded && <Result definition={definition} lightMode={lightMode} />}
     </div>
   );
 }
