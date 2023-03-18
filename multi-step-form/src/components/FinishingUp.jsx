4c6cd5ee48bda38b8bfe36917da664121e4a0d86
eytanbab <73233191+eytanbab@users.noreply.github.com>
2023-03-18 17:42:56 +0200

    update finisheing up component

--- multi-step-form/src/components/FinishingUp.jsx
+++ multi-step-form/src/components/FinishingUp.jsx
@@ -1,30 +1,14 @@
 import React from 'react';
 
-const FinishingUp = () => {
+const FinishingUp = (personalInfo, selectedPlan) => {
   return (
     <>
-      <h1 className='text-2xl font-bold text-blue-900'>Personal info</h1>
+      <h1 className='text-2xl font-bold text-blue-900'>Finishing up</h1>
       <p className=' text-neutral-400'>
-        Please provide your name, email address, and phone number.
+        Double-check everything looks OK before confirming.
       </p>
       {/* FORM INPUTS */}
-      <form className='flex flex-col py-4 font-medium text-blue-900 '>
-        <label className='text-sm'>Name</label>
-        <input
-          className='mb-4 w-full rounded-md border border-gray-300 p-2 outline-none'
-          placeholder='e.g. Stephen King'
-        />
-        <label className='text-sm'>Email Address</label>
-        <input
-          className='mb-4 w-full rounded-md border border-gray-300 p-2 outline-none'
-          placeholder='e.g. stephenking@lorem.com'
-        />
-        <label className='text-sm'>Phone Number</label>
-        <input
-          className='mb-4 w-full rounded-md border border-gray-300 p-2 outline-none'
-          placeholder='e.g. +1 234 567 890'
-        />
-      </form>
+      <div className='flex flex-col bg-red-500 py-4 font-medium text-blue-900 '></div>
     </>
   );
 };
