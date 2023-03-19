03d8b11e1a1a1ae66e846ef3f185b09d27176493
eytanbab <73233191+eytanbab@users.noreply.github.com>
2023-03-19 03:04:08 +0200

    updated

--- url-shortener-2/src/App.jsx
+++ url-shortener-2/src/App.jsx
@@ -1,35 +1,55 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
-import Input from './components/Input';
-import Info from './components/Info';
+import Results from './components/Results';
 
 function App() {
-  const [data, setData] = useState({});
-  const [url, setUrl] = useState('google.com');
+  const [data, setData] = useState([]); //ARRAY OF OBJECTS
+  const [url, setUrl] = useState('');
   const [isLoading, setIsLoading] = useState(true);
   const [shortenUrl, setShortenUrl] = useState('');
+  const inputRef = useRef(null);
+  const [error, setError] = useState(true);
 
   const getData = () => {
+    setIsLoading(true);
+    setError(true);
+    console.log('in get data...');
     fetch(`https://api.shrtco.de/v2/shorten?url=${url}`)
       .then((res) => res.json())
-      .then((result) => setData(result))
-      .catch((e) => console.error(e))
+      .then((actualData) => {
+        if (actualData.ok === true) {
+          setShortenUrl(actualData.result.short_link);
+          setError(false);
+        } else {
+          setError(true);
+        }
+      })
+      .catch((err) => console.error(err))
       .finally(() => setIsLoading(false));
   };
 
   useEffect(() => {
-    getData();
-    setShortenUrl(data?.result?.short_link);
+    url ? getData() : console.log('no url');
   }, [url]);
 
   return (
-    <>
+    <div className='relative w-screen overflow-hidden text-center'>
       <NavBar />
-      <Hero />
-      <Input />
-      <Info />
-    </>
+      <Hero inputRef={inputRef} />
+      {
+        <Results
+          isLoading={isLoading}
+          url={url}
+          setUrl={setUrl}
+          shortenUrl={shortenUrl}
+          setShortenUrl={setShortenUrl}
+          inputRef={inputRef}
+          error={error}
+        />
+      }
+      {/* <Info /> */}
+    </div>
   );
 }
 
