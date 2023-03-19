e244adf4527fb37aa580d6366cd0cb629a2c3ba6
eytanbab <73233191+eytanbab@users.noreply.github.com>
2023-03-19 23:07:37 +0200

    added about and footer

--- url-shortener-2/src/App.jsx
+++ url-shortener-2/src/App.jsx
@@ -3,17 +3,21 @@ import NavBar from './components/NavBar';
 import Hero from './components/Hero';
 import Results from './components/Results';
 import Info from './components/Info';
+import Footer from './components/Footer';
+import About from './components/About';
 
 function App() {
   const inputRef = useRef(null);
 
   return (
-    <div className='relative flex w-screen flex-col items-center overflow-hidden text-center'>
-      <div className='max-w-sm'>
+    <div className='relative flex min-h-screen w-screen flex-col items-center overflow-x-hidden text-center'>
+      <div className='max-w-md overflow-x-hidden'>
         <NavBar />
         <Hero inputRef={inputRef} />
         <Results inputRef={inputRef} />
         <Info />
+        <Footer inputRef={inputRef} />
+        <About />
       </div>
     </div>
   );
