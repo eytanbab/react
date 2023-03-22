f0b1713ebfbe2eebb1283ae13fb7e934eadfbc02
eytanbab <73233191+eytanbab@users.noreply.github.com>
2023-03-22 06:32:51 +0200

    added nav arrows

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,22 +1,23 @@
 import { useState } from 'react';
+import { AiOutlineArrowRight, AiOutlineArrowLeft } from 'react-icons/ai';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className=' h-screen w-screen bg-slate-900 p-4 text-slate-50'>
-      <div className='grid  w-full auto-rows-fr  grid-cols-3 place-items-center content-start items-start '>
+    <div className=' h-screen w-screen bg-slate-900 text-slate-50'>
+      <div className='grid max-h-[50] min-h-[50vh]  w-full grid-cols-3 place-items-center content-start items-start'>
         {/* TITLE */}
-        <h1 className='col-span-4 flex h-full w-full  items-center self-center border  px-12 text-7xl font-bold'>
+        <h1 className='col-span-4 flex h-full w-full  items-center self-center border  p-12 px-12 text-7xl font-bold'>
           My Portfolio
         </h1>
         {/* PROJECTS PREVIEW */}
-        <div className='col-span-2 row-span-2 h-full w-full place-self-start border'>
-          <img
-            src='https://images.unsplash.com/photo-1661956602944-249bcd04b63f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
-            className='object-cover '
-          />
-        </div>
+
+        <img
+          src='https://images.unsplash.com/photo-1661956602944-249bcd04b63f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
+          className='col-span-2 row-span-2'
+        />
+
         {/* DESCRIPTION */}
         <div className='row-span-2 grid h-full grid-cols-2 gap-2 border p-4'>
           <h1 className='col-span-2 text-3xl font-bold'>Project Name:</h1>
@@ -33,12 +34,10 @@ function App() {
             nostrum ad repellat sed consequatur quo nisi quod.
           </p>
           {/* ARROWS */}
-          <h1 className='flex items-center justify-center bg-red-500'>
-            Arrow left
-          </h1>
-          <h1 className='flex items-center justify-center bg-red-500'>
-            Arrow right
-          </h1>
+        </div>
+        <div className='col-span-3 flex h-96 w-full items-center justify-between bg-red-500 px-12'>
+          {<AiOutlineArrowLeft className='h-60 w-60 text-slate-50 ' />}
+          {<AiOutlineArrowRight className='h-60 w-60 text-slate-50' />}
         </div>
 
         <div className='col-span-1 border '>
@@ -46,7 +45,7 @@ function App() {
         </div>
       </div>
       {/* FOOTER */}
-      <div className='fixed bottom-0 flex h-12 w-full items-center justify-center bg-red-500 p-12'>
+      <div className='fixed bottom-0 flex w-full items-center justify-center p-12'>
         <h1 className='flex items-center justify-center text-7xl uppercase'>
           this is a footer
         </h1>
