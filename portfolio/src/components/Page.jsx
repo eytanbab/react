aa06d065d7a56196706da0714fd97d2427895941
eytanbab <73233191+eytanbab@users.noreply.github.com>
2023-03-23 17:00:25 +0200

    added transition

--- portfolio/src/components/Page.jsx
+++ portfolio/src/components/Page.jsx
@@ -6,24 +6,51 @@ const Page = ({ data }) => {
   const isInView = useInView(ref, { once: true });
   return (
     <div
-      className='grid-col-1 grid-row-5 grid items-start justify-center gap-6 text-center'
+      className='grid-col-1 grid-row-5 grid items-start justify-center gap-6 overflow-hidden text-center'
       ref={ref}
-      style={{
-        transform: isInView ? 'none' : 'translateX(-200px)',
-        opacity: isInView ? 1 : 0,
-        transition: 'all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s',
-      }}
     >
       <img
         src='https://images.unsplash.com/photo-1661956602944-249bcd04b63f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
         className='row-span-1 row-start-1 object-fill'
+        style={{
+          transform: isInView ? 'none' : 'translateX(-200px)',
+          opacity: isInView ? 1 : 0,
+          transition: 'all 0.3s cubic-bezier(0.17, 0.55, 0.55, 1) 0.3s',
+          filter: isInView ? 'none' : 'blur(30px)',
+        }}
       />
-      <h1 className='text-3xl font-medium'>{data.projectName}</h1>
-      <p className=' text-slate-50 '>{data.projectDesc}</p>
+      <h1
+        className='text-3xl font-medium'
+        style={{
+          transform: isInView ? 'none' : 'translateX(200px)',
+          opacity: isInView ? 1 : 0,
+          transition: 'all 0.3s cubic-bezier(0.17, 0.55, 0.55, 1) 0.6s',
+          filter: isInView ? 'none' : 'blur(5px)',
+        }}
+      >
+        {data.projectName}
+      </h1>
+      <p
+        className=' text-slate-50 '
+        style={{
+          transform: isInView ? 'none' : 'translateX(-200px)',
+          opacity: isInView ? 1 : 0,
+          transition: 'all 0.3s cubic-bezier(0.17, 0.55, 0.55, 1) 0.9s',
+          filter: isInView ? 'none' : 'blur(5px)',
+        }}
+      >
+        {data.projectDesc}
+      </p>
       {data.link ? (
         <a
           href={data.link}
           className='justify-self-center rounded-xl border border-slate-50/50 p-1 px-2  text-lg'
+          style={{
+            transform: isInView ? 'none' : 'translateX(-200px)',
+            opacity: isInView ? 1 : 0,
+            transition: 'all 0.3s cubic-bezier(0.17, 0.55, 0.55, 1) 0.9s',
+            filter: isInView ? 'none' : 'blur(5px)',
+          }}
         >
           Live
         </a>
