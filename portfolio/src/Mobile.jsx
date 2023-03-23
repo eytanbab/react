20c184079dcfda09a6922329828e031c644fb3f9
eytanbab <73233191+eytanbab@users.noreply.github.com>
2023-03-23 18:40:39 +0200

    added disabled to navigation buttons

--- portfolio/src/Mobile.jsx
+++ portfolio/src/Mobile.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
+import './App.css';
 import Page from './components/Page';
 import Footer from './components/Footer';
 import data from './data';
@@ -7,29 +7,49 @@ import Navbar from './components/Navbar';
 
 const Mobile = () => {
   const [pageIndex, setPageIndex] = useState(1);
+  const [nextDisabled, setNextDisabled] = useState(false);
+  const [prevDisabled, setPrevDisabled] = useState(false);
 
   const pageNextHandle = () => {
-    if (pageIndex >= data.length) return;
     setPageIndex((prev) => prev + 1);
   };
 
   const pagePrevHandle = () => {
-    if (pageIndex <= 1) return;
     setPageIndex((prev) => prev - 1);
   };
 
+  useEffect(() => {
+    if (pageIndex === 1) {
+      setPrevDisabled(true);
+    } else {
+      setPrevDisabled(false);
+    }
+    if (pageIndex === data.length) {
+      setNextDisabled(true);
+    } else {
+      setNextDisabled(false);
+    }
+  }, [pageIndex]);
+
   return (
-    <div className='flex  h-screen flex-col items-center justify-start gap-4 bg-slate-900 p-6 text-slate-100'>
+    <div className='flex h-screen flex-col items-center justify-start gap-4 bg-slate-900 p-6 text-slate-100'>
       <Navbar />
       <div className='flex h-screen flex-col justify-between'>
         {data.map((data, index) =>
-          index === pageIndex - 1 ? <Page key={index} data={data} /> : ''
+          index === pageIndex - 1 ? (
+            <Page
+              key={index}
+              data={data}
+              pagePrevHandle={pagePrevHandle}
+              pageNextHandle={pageNextHandle}
+              nextDisabled={nextDisabled}
+              prevDisabled={prevDisabled}
+            />
+          ) : (
+            ''
+          )
         )}
-        {/* BUTTONS AND FOOTER DIV */}
-        <Footer
-          pagePrevHandle={pagePrevHandle}
-          pageNextHandle={pageNextHandle}
-        />
+        <Footer />
       </div>
     </div>
   );
