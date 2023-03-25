863b6aaa4b28598af4cb379cf26282b58b2a0242
eytanbab <73233191+eytanbab@users.noreply.github.com>
2023-03-25 16:54:01 +0300

    fixed mobile nav

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -21,15 +21,17 @@ const Navbar = () => {
         ) : (
           <RxHamburgerMenu onClick={handleNav} />
         )}
-        <div
-          className={`${
-            isOpen ? 'left-0' : '-left-[50rem]'
-          } absolute left-0 top-16 z-10 flex h-[80vh] w-full flex-col items-start gap-4 rounded-lg bg-slate-800 p-6 transition-all duration-300 ease-in`}
-        >
-          <button>Projects</button>
-          <div className='h-[1px] w-full bg-slate-200/50' />
-          <button>About</button>
-        </div>
+        {isOpen && (
+          <div
+            className={`${
+              isOpen ? 'left-0' : '-left-[50rem]'
+            } absolute left-0 top-16 z-10 flex h-[80vh] w-full flex-col items-start gap-4 rounded-lg bg-slate-800 p-6 transition-all duration-300 ease-in`}
+          >
+            <button>Projects</button>
+            <div className='h-[1px] w-full bg-slate-200/50' />
+            <button>About</button>
+          </div>
+        )}
       </ul>
     </nav>
   );
