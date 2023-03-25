9bd308728b920e145f24ff98e28704ec94d32f80
eytanbab <73233191+eytanbab@users.noreply.github.com>
2023-03-25 18:21:00 +0300

    Added about

---
+++ portfolio/src/components/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className='bg-red-500'>
+      <p>
+        Hey! my name is Ethan Babaev, and I am a 27-year-old software
+        engineering graduate from Sami Shamoon College of Engineering. As a
+        recent graduate, I am excited to start my career in the field of
+        frontend development. Though I don't have any professional experience
+        yet, I have been honing my skills in coding and design through various
+        personal projects and coursework. I am proficient in HTML, CSS,
+        JavaScript, and React and have a keen eye for detail and aesthetics. I
+        am also a quick learner and thrive in collaborative environments. I am
+        currently seeking opportunities to work as a frontend developer and
+        contribute to innovative projects. I am confident that my skills and
+        passion for coding make me a valuable addition to any team. Thank you
+        for visiting my portfolio, and I look forward to connecting with you.
+      </p>
+    </div>
+  );
+};
+
+export default About;
