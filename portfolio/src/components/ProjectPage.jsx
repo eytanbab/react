223eb4dae94b9301bb0a5d337028af44bc1390dc
eytanbab <73233191+eytanbab@users.noreply.github.com>
2023-03-26 20:14:05 +0300

    updated images

--- portfolio/src/components/ProjectPage.jsx
+++ portfolio/src/components/ProjectPage.jsx
@@ -21,7 +21,7 @@ const ProjectPage = ({
       <img
         src={data.projectImageUrl}
         alt='project img'
-        className='col-span-1 row-span-1 row-start-1  object-contain xl:col-span-2 xl:row-span-3'
+        className='col-span-1 row-span-1 row-start-1  object-fill xl:col-span-2 xl:row-span-3'
         style={{
           transform: isInView ? 'none' : 'translateX(-200px)',
           opacity: isInView ? 1 : 0,
--- portfolio/src/data.js
+++ portfolio/src/data.js
@@ -1,3 +1,4 @@
+import img from './assets/advice-generator.png';
 const data = [
   {
     projectId: 1,
@@ -5,9 +6,8 @@ const data = [
     projectDesc:
       'Get instant guidance with our advice generator app! Simply click the button and receive a random piece of advice, based on expert insights and proven psychological principles.',
     techUsed: [],
-    projectImageUrl:
-      'https://images.unsplash.com/photo-1661956602944-249bcd04b63f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
-    link: '',
+    projectImageUrl: 'https://i.ibb.co/5rV5yBt/advice-generator.png',
+    link: 'https://advice-generator-7.netlify.app/',
     repo: 'https://github.com/eytanbab/react/tree/master/advice-generator',
   },
   {
@@ -16,8 +16,7 @@ const data = [
     projectDesc:
       'Find word meanings quickly with our easy-to-use dictionary web app. Access millions of words and definitions, stay up-to-date with the latest language trends, and start exploring now!',
     techUsed: [],
-    projectImageUrl:
-      'https://images.unsplash.com/photo-1661956602944-249bcd04b63f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+    projectImageUrl: 'https://i.ibb.co/9gsPMzX/dictionary.png',
     link: 'https://dict-web-app.netlify.app/',
     repo: 'https://github.com/eytanbab/react/tree/master/dictionary-web-app',
   },
@@ -38,8 +37,7 @@ const data = [
     projectDesc:
       'Tired of the hassle of calculating tips? Our tip calculator app is here to help! With just a few taps, you can easily split the bill and calculate the perfect tip for your meal. ',
     techUsed: [],
-    projectImageUrl:
-      'https://images.unsplash.com/photo-1661956602944-249bcd04b63f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+    projectImageUrl: 'https://i.ibb.co/cDWJDw1/tip-calculator.png',
     link: 'https://calculate-my-tip.netlify.app/',
     repo: 'https://github.com/eytanbab/react/tree/master/tip-calculator-app',
   },
@@ -49,8 +47,7 @@ const data = [
     projectDesc:
       'Introducing our URL shortener - the perfect tool for simplifying and streamlining your online presence! With our app, you can quickly and easily generate shortened links that are easy to remember and share.',
     techUsed: [],
-    projectImageUrl:
-      'https://images.unsplash.com/photo-1661956602944-249bcd04b63f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+    projectImageUrl: 'https://i.ibb.co/7nCBj0H/shortly.png',
     link: 'https://url-shortener-2.netlify.app/',
     repo: 'https://github.com/eytanbab/react/tree/master/url-shortener-2',
   },
@@ -60,8 +57,7 @@ const data = [
     projectDesc:
       "Stay ahead of the weather with our user-friendly web app! With real-time updates and accurate forecasts, you'll always be prepared for whatever Mother Nature has in store. ",
     techUsed: [],
-    projectImageUrl:
-      'https://images.unsplash.com/photo-1661956602944-249bcd04b63f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+    projectImageUrl: 'https://i.ibb.co/vzCx0yF/weatherly.png',
     link: 'https://weatherly-web-app.netlify.app/',
     repo: 'https://github.com/eytanbab/react/tree/master/weather-web-app',
   },
