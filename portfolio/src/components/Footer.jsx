cd9b133a3d4ec71bdc69dc347190b4bc0fed46c3
eytanbab <73233191+eytanbab@users.noreply.github.com>
2023-03-26 17:51:01 +0300

    added aria labels

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -4,10 +4,10 @@ import { RiLinkedinLine } from 'react-icons/ri';
 const Footer = () => {
   return (
     <div className='flex h-24 w-full items-center justify-center gap-4 p-4 text-3xl'>
-      <a href='https://github.com/eytanbab'>
+      <a href='https://github.com/eytanbab' aria-label='GitHub'>
         <AiFillGithub />
       </a>
-      <a href='https://www.linkedin.com/in/eytan-babaev/'>
+      <a href='https://www.linkedin.com/in/eytan-babaev/' aria-label='LinkedIn'>
         <RiLinkedinLine />
       </a>
     </div>
