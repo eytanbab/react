7d8473dcc8bd99665c90074063eecc0aa4206eea
eytanbab <73233191+eytanbab@users.noreply.github.com>
2023-03-28 16:08:28 +0300

    fixed colors of each row

--- crypto-web/src/components/Coin.jsx
+++ crypto-web/src/components/Coin.jsx
@@ -12,7 +12,7 @@ const Coin = ({ coin, index }) => {
       <button
         onClick={handleModal}
         className={`${
-          index % 2 === 0 ? 'bg-electric-800' : ''
+          coin.rank % 2 === 0 ? 'bg-electric-800' : ''
         } grid w-full grid-cols-5 items-center justify-center p-4 `}
       >
         <div className='col-span-2 flex items-center gap-2 py-1'>
