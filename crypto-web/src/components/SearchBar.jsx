f85941bf3fee35a529fbbbc0f4a5b43b3f1197a1
eytanbab <73233191+eytanbab@users.noreply.github.com>
2023-03-28 18:54:54 +0300

    added onsubmit

--- crypto-web/src/components/SearchBar.jsx
+++ crypto-web/src/components/SearchBar.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
+import { useState } from 'react';
 import { AiOutlineSearch } from 'react-icons/ai';
 
 const SearchBar = ({ setQuery }) => {
+  const [tempQuery, setTempQuery] = useState('');
   const handleChange = (e) => {
+    setTempQuery(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    setQuery(e.target.value);
+    setQuery(tempQuery);
   };
 
   return (
-    <form className='flex items-center justify-between border-b border-b-electric-200/80 bg-transparent'>
+    <form
+      className='flex items-center justify-between border-b border-b-electric-200/80 bg-transparent'
+      onSubmit={(e) => handleSubmit(e)}
+    >
       <input
         className='w-full bg-transparent p-2 outline-none placeholder:text-electric-200'
         type='text'
         placeholder='Search crypto...'
         onChange={handleChange}
       />
-      <AiOutlineSearch size={30} />
+      <button type='submit'>
+        <AiOutlineSearch size={30} />
+      </button>
     </form>
   );
 };
