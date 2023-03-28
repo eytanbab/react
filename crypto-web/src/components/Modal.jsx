d286f778540e975816ecf1e7b22e67c3795c732f
eytanbab <73233191+eytanbab@users.noreply.github.com>
2023-03-28 16:46:14 +0300

    fixed modal positioning and added ring

--- crypto-web/src/components/Modal.jsx
+++ crypto-web/src/components/Modal.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
-
 const Modal = ({ coin, setOpenModal }) => {
   return (
-    <div className='absolute inset-0  flex h-screen w-full  items-center justify-center backdrop-blur-sm'>
+    <div className='fixed inset-0  z-50 flex h-screen w-full items-center justify-center backdrop-blur-sm'>
       {/* MODAL */}
-      <div className='flex h-[70vh] w-96 flex-col items-center justify-between gap-6 overflow-y-hidden rounded-md bg-electric-800 p-4'>
+      <div className='flex h-[60vh] w-80 flex-col items-center justify-between gap-6 rounded-md bg-electric-800 p-4 ring ring-electric-300/80'>
         <div className='flex w-full grow flex-col items-center gap-4'>
           <img src={coin.iconUrl} className='h-20 w-20' />
           <h1 className='text-lg font-bold'>{coin.name}</h1>
           <h1>Rank: {coin.rank}</h1>
           <h1>{coin.symbol}</h1>
-          <h1>Price: {parseFloat(coin.price).toFixed(2)}$</h1>
+          <h1>
+            Price:{' '}
+            {parseFloat(parseFloat(coin.price).toFixed(2)).toLocaleString()}$
+          </h1>
           <h1>Market Cap: {parseFloat(coin.marketCap).toLocaleString()}$</h1>
           <h1>
             Change:{' '}
