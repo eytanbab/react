9964d2aed9ec58506491503ee684c82bbeb753ad
eytanbab <73233191+eytanbab@users.noreply.github.com>
2023-04-03 18:01:29 +0300

    added rating component

---
+++ components/src/components/Rating.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { AiFillStar } from 'react-icons/ai';
+
+const Rating = () => {
+  return (
+    <div className='flex gap-2'>
+      <AiFillStar />
+      <AiFillStar />
+      <AiFillStar />
+      <AiFillStar />
+      <AiFillStar />
+    </div>
+  );
+};
+
+export default Rating;
