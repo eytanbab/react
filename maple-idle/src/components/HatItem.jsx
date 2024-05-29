899c6ae7c23f2bfbac158f65cb0863ec16ceb0c1
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-05-29 18:49:13 +0300

    feat: Add items components

---
+++ maple-idle/src/components/HatItem.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const HatItem = ({ item, coins, setCurrentHat }) => {
+  // check if there are enough coins for upgrading
+  const canUpgrade = coins >= item.price;
+
+  const upgradeHandle = () => {
+    canUpgrade && setCurrentHat((prev) => prev + 1);
+  };
+
+  return (
+    <div className='w-40 flex flex-col bg-gray-100 text-gray-900 uppercase rounded-xl border-4 border-gray-900'>
+      <h1 className='bold uppercase w-full font-bold text-lg text-center'>
+        Hat
+      </h1>
+      <img src={item.imgURL} className='w-full' />
+      {/* INFO */}
+      <div className='flex flex-col gap-1 px-2 py-1'>
+        <p>cost: {item.price}</p>
+        <p>Level: {item.reqLevel}</p>
+        <div className='grid grid-rows-2 grid-cols-2 w-full '>
+          <p>str:{item.stats.str}</p>
+          <p>dex:{item.stats.dex}</p>
+          <p>int:{item.stats.int}</p>
+          <p>luk:{item.stats.luk}</p>
+        </div>
+      </div>
+      <button
+        // onClick={upgradeHandle}
+        className={`${
+          canUpgrade ? 'bg-gray-900 text-gray-100' : 'bg-gray-300 text-gray-600'
+        } w-full py-2 px-1 rounded-lg ring-4 ring-gray-900 font-bold`}
+      >
+        Upgrade
+      </button>
+    </div>
+  );
+};
+
+export default HatItem;
