3c439469516ca4ccb1a46c693f04951167c69faf
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-05-29 18:49:21 +0300

    feat: Add placeholder data

---
+++ maple-idle/src/utils/data.js
@@ -0,0 +1,63 @@
+export const swords = [
+  {
+    reqLevel: 1,
+    imgURL:
+      'https://global.hidden-street.net/sites/global.hidden-street.net/files/eqs/onehandsw003.gif?1243548939',
+    price: 10,
+    stats: {
+      str: 5,
+      dex: 2,
+      int: 0,
+      luk: 0,
+    },
+    tapMultiplier: 1,
+  },
+];
+
+export const hats = [
+  {
+    reqLevel: 1,
+    imgURL:
+      'https://www.hidden-street.net/sites/default/files/images/equipment/7f83efb4750c10a024f3f4692fb210cb.png',
+    price: 0,
+    stats: {
+      str: 5,
+      dex: 2,
+      int: 0,
+      luk: 0,
+    },
+    tapMultiplier: 1,
+  },
+];
+
+export const tops = [
+  {
+    reqLevel: 1,
+    imgURL:
+      'https://www.hidden-street.net/sites/default/files/images/equipment/00f230ffad56df32a9da6c837448cdbc.png',
+    price: 0,
+    stats: {
+      str: 5,
+      dex: 2,
+      int: 0,
+      luk: 0,
+    },
+    tapMultiplier: 1,
+  },
+];
+
+export const bottoms = [
+  {
+    reqLevel: 1,
+    imgURL:
+      'https://www.hidden-street.net/sites/default/files/images/equipment/636e8c4faad4fde67001ec06f8daf8a1.png',
+    price: 0,
+    stats: {
+      str: 5,
+      dex: 2,
+      int: 0,
+      luk: 0,
+    },
+    tapMultiplier: 1,
+  },
+];
