6eba2787e6c75d1aef4109223c50ba373a520bca
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-08-06 19:32:25 +0300

    feat: Add destination placeholder

--- space-tourism-3/src/App.tsx
+++ space-tourism-3/src/App.tsx
@@ -1,13 +1,15 @@
 import Home from './components/Home';
 import MobileNavBar from './components/Navbar/MobileNavBar';
+import Destination from './components/Pages/Destination';
 
 function App() {
   return (
     <div className={` text-white flex flex-col p-6 h-screen`}>
       {/* NAVBAR */}
       <MobileNavBar />
-      {/* HERO SECITON */}
-      <Home />
+      {/* PAGES SECITON */}
+      {/* <Home /> */}
+      <Destination />
     </div>
   );
 }
