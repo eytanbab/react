5ae1577f36de0ceaf84b3f7ee0a17d79e810ac5f
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-08-09 19:38:23 +0300

    refactor: Change import order

--- space-tourism-3/src/App.tsx
+++ space-tourism-3/src/App.tsx
@@ -1,7 +1,7 @@
-import Home from './components/Home';
 import MobileNavBar from './components/Navbar/MobileNavBar';
-import Crew from './components/Pages/Crew';
+import Home from './components/Home';
 import Destination from './components/Pages/Destination';
+import Crew from './components/Pages/Crew';
 
 function App() {
   return (
