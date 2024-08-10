f379c928d7041c64d70649d2f254534efa38e27c
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-08-10 19:59:22 +0300

    feat: Add router paths

--- space-tourism-3/src/App.tsx
+++ space-tourism-3/src/App.tsx
@@ -1,4 +1,6 @@
 import MobileNavBar from './components/Navbar/MobileNavBar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Home from './components/Home';
 import Destination from './components/Pages/Destination';
 import Crew from './components/Pages/Crew';
@@ -7,13 +9,15 @@ import Technologies from './components/Pages/Technologies';
 function App() {
   return (
     <div className={` text-white flex flex-col h-screen`}>
-      {/* NAVBAR */}
-      <MobileNavBar />
-      {/* PAGES SECITON */}
-      {/* <Home /> */}
-      {/* <Destination /> */}
-      {/* <Crew /> */}
-      <Technologies />
+      <BrowserRouter>
+        <MobileNavBar />
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/destination' element={<Destination />}></Route>
+          <Route path='/crew' element={<Crew />}></Route>
+          <Route path='/technology' element={<Technologies />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
