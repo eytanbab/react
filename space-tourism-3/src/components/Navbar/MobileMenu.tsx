af8068ad6ca8a6ac234fa3988827fdcf649d6644
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-08-11 19:56:29 +0300

    refactor: Use links array for NavLink mapping

--- space-tourism-3/src/components/Navbar/MobileMenu.tsx
+++ space-tourism-3/src/components/Navbar/MobileMenu.tsx
@@ -1,5 +1,16 @@
 import { NavLink } from 'react-router-dom';
 
+type Link = {
+  name: string;
+  path: string;
+};
+const links: Link[] = [
+  { name: 'home', path: '/' },
+  { name: 'destination', path: '/destination' },
+  { name: 'crew', path: '/crew' },
+  { name: 'technology', path: '/technology' },
+];
+
 const MobileMenu = ({
   setIsOpenNav,
 }: {
@@ -10,43 +21,22 @@ const MobileMenu = ({
   };
   return (
     <div className='absolute inset-0 h-full w-full bg-black flex items-center justify-center text-xl uppercase z-10'>
-      <div className='flex flex-col items-start gap-8 px-12 w-full'>
-        <NavLink
-          to='/'
-          onClick={closeNavHandler}
-          className={({ isActive }) => (isActive ? 'border-r-2 w-full' : '')}
-        >
-          <p className='flex gap-x-2'>
-            <span className='font-bold'>00</span> Home
-          </p>
-        </NavLink>
-        <NavLink
-          to='/destination'
-          onClick={closeNavHandler}
-          className={({ isActive }) => (isActive ? 'border-r-2 w-full' : '')}
-        >
-          <p className='flex gap-x-2'>
-            <span className='font-bold'>01</span> Destination
-          </p>
-        </NavLink>
-        <NavLink
-          to='/crew'
-          onClick={closeNavHandler}
-          className={({ isActive }) => (isActive ? 'border-r-2 w-full' : '')}
-        >
-          <p className='flex gap-x-2'>
-            <span className='font-bold'>02</span> Crew
-          </p>
-        </NavLink>
-        <NavLink
-          to='/technology'
-          onClick={closeNavHandler}
-          className={({ isActive }) => (isActive ? 'border-r-2 w-full' : '')}
-        >
-          <p className='flex gap-x-2'>
-            <span className='font-bold'>03</span> Technology
-          </p>
-        </NavLink>
+      <div className='flex flex-col items-start gap-8 pl-12 w-full'>
+        {links.map((link, idx) => {
+          return (
+            <NavLink
+              to={link.path}
+              onClick={closeNavHandler}
+              className={({ isActive }) =>
+                isActive ? 'border-r-4 w-full' : 'w-full'
+              }
+            >
+              <p className='flex gap-x-4'>
+                <span className='font-bold'>{`0${idx + 1}`}</span> {link.name}
+              </p>
+            </NavLink>
+          );
+        })}
       </div>
     </div>
   );
