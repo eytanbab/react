72b4c68a0f4329332c06f99b14a837bf7260ab43
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-08-13 19:41:02 +0300

    feat: import tablet navigation bar

--- space-tourism-3/src/App.tsx
+++ space-tourism-3/src/App.tsx
@@ -1,16 +1,21 @@
-import MobileNavBar from './components/Navbar/MobileNavBar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MobileNavBar from './components/Navbar/MobileNavBar';
 
 import Home from './components/Pages/Home';
 import Destination from './components/Pages/Destination';
 import Crew from './components/Pages/Crew/Crew';
 import Technologies from './components/Pages/Technologies';
+import TabletNavBar from './components/Navbar/TabletNavBar';
 
 function App() {
   return (
     <div className={` text-white flex flex-col h-screen`}>
       <BrowserRouter>
+        {/* WIDTH < 768px */}
         <MobileNavBar />
+        {/* WIDTH >= 768px */}
+        <TabletNavBar />
+
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path='/destination' element={<Destination />}></Route>
