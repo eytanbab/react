944307f7e8da1ef206fdc9d3c9bc6fbaef0b2fc0
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-08-13 19:40:44 +0300

    chore: import nav links from constants file

--- space-tourism-3/src/components/Navbar/MobileMenu.tsx
+++ space-tourism-3/src/components/Navbar/MobileMenu.tsx
@@ -1,15 +1,5 @@
 import { NavLink } from 'react-router-dom';
-
-type Link = {
-  name: string;
-  path: string;
-};
-const links: Link[] = [
-  { name: 'home', path: '/' },
-  { name: 'destination', path: '/destination' },
-  { name: 'crew', path: '/crew' },
-  { name: 'technology', path: '/technology' },
-];
+import { links } from '../../utils/constants';
 
 const MobileMenu = ({
   setIsOpenNav,
