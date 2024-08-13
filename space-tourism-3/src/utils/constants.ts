51f4ab878c5c7a19dcda743bfad83e46b4560147
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-08-13 19:39:22 +0300

    chore: move nav links to constants file

---
+++ space-tourism-3/src/utils/constants.ts
@@ -0,0 +1,11 @@
+type Link = {
+  name: string;
+  path: string;
+};
+
+export const links: Link[] = [
+  { name: 'home', path: '/' },
+  { name: 'destination', path: '/destination' },
+  { name: 'crew', path: '/crew' },
+  { name: 'technology', path: '/technology' },
+];
