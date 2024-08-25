f3f19cb879119d2441b48535392e9d34602c27cd
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-08-25 17:57:04 +0300

    feat: add name and attack to Item type

--- maple-idle/src/types/item.ts
+++ maple-idle/src/types/item.ts
@@ -2,9 +2,11 @@ import { Stats } from './stats';
 
 export type Item = {
   reqLevel: number;
+  name: string;
   imgURL: string;
   price: number;
   stats?: Stats;
+  attack?: number;
   expMultiplier: number;
   mesosMultiplier: number;
 };
