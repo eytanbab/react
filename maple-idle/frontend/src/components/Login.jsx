188b06b5b267efb48ee2ceb58c2395bb33976309
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-08-27 00:18:33 +0300

    feat: add bg image and login button

--- maple-idle/frontend/src/components/Login.jsx
+++ maple-idle/frontend/src/components/Login.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { SignedOut, SignInButton } from '@clerk/clerk-react';
 
 const Login = () => {
   return (
-    <div className='bg-[url("./bg.jpg")] bg-cover bg-no-repeat w-full h-screen flex items-center justify-center text-3xl'>
-      
+    <div className='bg-[url("./bg.jpg")] bg-cover bg-no-repeat w-full h-screen flex items-center justify-center'>
+      <div className='bg-[url("./wood-pluck.jpg")] bg-cover bg-no-repeat w-full text-center text-white p-4 rounded-3xl flex items-center justify-center m-4 shadow-md'>
+        <SignedOut>
+          <SignInButton className='uppercase  text-6xl shadow-slate-500 [text-shadow:_0_4px_8px_rgb(0_0_0_/_100%)]' />
+        </SignedOut>
+      </div>
     </div>
   );
 };
