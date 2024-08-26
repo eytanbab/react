7633377f950ec5436e386fa8b109219531721b5d
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-08-27 00:19:40 +0300

    feat: add user authentication status check with Clerk

--- maple-idle/frontend/src/App.jsx
+++ maple-idle/frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import MainMenu from './components/MainMenu';
 import Login from './components/Login';
+import { useUser } from '@clerk/clerk-react';
 
 const App = () => {
-  return (
-    <>
-      <Login />
-      {/* <MainMenu /> */}
-    </>
-  );
+  const { isSignedIn } = useUser();
+  if (isSignedIn) return <MainMenu />;
+  return <Login />;
 };
 
 export default App;
