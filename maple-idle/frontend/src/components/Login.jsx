881eb81e239794bdbb4e040a7ee04ec886125c90
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-08-26 23:46:59 +0300

    feat: create login component

---
+++ maple-idle/frontend/src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className='bg-[url("./bg.jpg")] bg-cover bg-no-repeat w-full h-screen flex items-center justify-center text-3xl'>
+      
+    </div>
+  );
+};
+
+export default Login;
