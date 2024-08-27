16a13dbf9eff6755eb5f955567e55aa83754570c
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-08-27 23:45:17 +0300

    feat: add maps init data

---
+++ maple-idle/frontend/src/data/maps.ts
@@ -0,0 +1,45 @@
+export const maps = [
+  // LEVEL 1-9
+  {
+    mapLevel: 1,
+    mapName: 'Right Around Lith Harbor',
+    mapURL: './right-around-lith-harbor.webp',
+    monsters: [
+      {
+        monsterName: 'Snail',
+        monsterURL: './snail.webp',
+        monsterExp: 3,
+        monsterMesos: 4,
+      },
+      {
+        monsterName: 'Red Snail',
+        monsterURL: './red-snail.webp',
+        monsterLevel: 5,
+        monsterExp: 8,
+        monsterMesos: 12,
+      },
+    ],
+  },
+  // LEVEL 10 19
+  {
+    mapLevel: 10,
+    mapName: 'Rocky Road I',
+    mapURL: './rocky-road.webp',
+    monsters: [
+      {
+        monsterName: 'Stump',
+        monsterURL: './stump.webp',
+        mosterLevel: 10,
+        monsterExp: 8,
+        monsterMesos: 12,
+      },
+      {
+        monsterName: 'Axe Stump',
+        monsterURL: './axe-stump.webp',
+        mosterLevel: 15,
+        monsterExp: 30,
+        monsterMesos: 32,
+      },
+    ],
+  },
+];
