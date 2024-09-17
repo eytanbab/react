e9530fe68c28d43c5de7b640f0690f18724a9a20
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-18 02:18:02 +0300

    feat: add nav

---
+++ markdown-editor/frontend/src/components/nav.tsx
@@ -0,0 +1,17 @@
+import { useGetSession } from '../hooks/use-get-session';
+import SignInButton from './sign-in-button';
+import ModeToggle from './mode-toggle';
+import SearchBar from './search-bar';
+
+const Nav = () => {
+  const [session] = useGetSession();
+  return (
+    <div className='w-full flex items-center justify-between p-4'>
+      {!session && <SignInButton />}
+      {session && <SearchBar />}
+      <ModeToggle />
+    </div>
+  );
+};
+
+export default Nav;
