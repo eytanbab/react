b37ce56c51945f21b8af6d8d8202ddc08994d03e
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-18 02:19:51 +0300

    feat: integrate theme provider and nav

--- markdown-editor/frontend/src/App.tsx
+++ markdown-editor/frontend/src/App.tsx
@@ -1,11 +1,18 @@
-function App() {
+import { ThemeProvider } from './components/theme-provider';
+import SideNav from './components/side-nav';
+import Separator from './components/separator';
+import Nav from './components/nav';
 
+const App = () => {
   return (
-    <div className='w-full h-screen bg-red-500'>
-      <h1>hello</h1>
-    
-    </div>
-  )
-}
+    <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
+      <div className='w-full h-screen flex items-start justify-start bg-slate-100 text-slate-950 dark:bg-slate-950 dark:text-slate-100'>
+        <SideNav />
+        <Separator />
+        <Nav />
+      </div>
+    </ThemeProvider>
+  );
+};
 
-export default App
+export default App;
