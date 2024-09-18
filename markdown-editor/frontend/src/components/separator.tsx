54456cd1efea73fe16a0c6d55be79babb086862f
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-18 18:18:10 +0300

    style: add flex-none to separator

--- markdown-editor/frontend/src/components/separator.tsx
+++ markdown-editor/frontend/src/components/separator.tsx
@@ -1,5 +1,7 @@
 const Separator = () => {
-  return <div className='h-full w-0.5 bg-slate-950/10 dark:bg-slate-200/10' />;
+  return (
+    <div className='h-full w-[2px] bg-slate-950/10 dark:bg-slate-200/10 flex-none' />
+  );
 };
 
 export default Separator;
