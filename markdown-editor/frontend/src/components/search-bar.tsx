fc97b59af1871aebb436980b9d5fff3ca02aab76
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-19 01:23:58 +0300

    style: change placeholder font weight to light

--- markdown-editor/frontend/src/components/search-bar.tsx
+++ markdown-editor/frontend/src/components/search-bar.tsx
@@ -2,7 +2,7 @@ const SearchBar = () => {
   return (
     <input
       placeholder='Search saved markdowns...'
-      className='rounded-full px-4 py-1 w-full min-w-0 max-w-80 outline-none bg-slate-50 dark:bg-slate-800 drop-shadow-md placeholder:text-slate-400'
+      className='rounded-full px-4 py-1 w-full min-w-0 max-w-80 outline-none bg-slate-50 dark:bg-slate-800 drop-shadow-md placeholder:text-slate-400 font-light'
     />
   );
 };
