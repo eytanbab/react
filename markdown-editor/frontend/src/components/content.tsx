7dce7b1f4a8b1a351f3c9adc3d71addf00af3881
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-18 18:15:12 +0300

    feat: add content placeholder component

---
+++ markdown-editor/frontend/src/components/content.tsx
@@ -0,0 +1,18 @@
+type Props = {
+  setMarkdown: (e: string) => void;
+};
+
+const Content = ({ setMarkdown }: Props) => {
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMarkdown(e.target.value);
+  };
+
+  return (
+    <textarea
+      onChange={handleChange}
+      className='flex-1 shrink-0 w-full border-none outline-none resize-none bg-slate-50 dark:bg-slate-900 p-4 rounded-xl drop-shadow-md'
+    />
+  );
+};
+
+export default Content;
