27dcbb7f6470a6ffa228f0727ccfab89149068b9
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-18 18:19:34 +0300

    style: fix overflow and layout

--- markdown-editor/frontend/src/App.tsx
+++ markdown-editor/frontend/src/App.tsx
@@ -2,14 +2,18 @@ import { ThemeProvider } from './components/theme-provider';
 import SideNav from './components/side-nav';
 import Separator from './components/separator';
 import Nav from './components/nav';
+import MarkdownEditor from './components/markdown-editor';
 
 const App = () => {
   return (
     <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
-      <div className='w-full h-screen flex items-start justify-start bg-slate-100 text-slate-950 dark:bg-slate-950 dark:text-slate-100'>
+      <div className='h-screen flex bg-slate-100 text-slate-950 dark:bg-slate-950 dark:text-slate-100'>
         <SideNav />
         <Separator />
-        <Nav />
+        <div className='flex flex-col w-full gap-x-2'>
+          <Nav />
+          <MarkdownEditor />
+        </div>
       </div>
     </ThemeProvider>
   );
