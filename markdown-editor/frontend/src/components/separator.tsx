da6dc2ea287482cb082272b4caad387f33141aa2
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-18 18:13:35 +0300

    style: change separator opacity

--- markdown-editor/frontend/src/components/separator.tsx
+++ markdown-editor/frontend/src/components/separator.tsx
@@ -1,5 +1,5 @@
 const Separator = () => {
-  return <div className='h-full w-0.5 bg-slate-950/25 dark:bg-slate-200/50' />;
+  return <div className='h-full w-0.5 bg-slate-950/10 dark:bg-slate-200/10' />;
 };
 
 export default Separator;
