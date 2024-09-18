8af0afab99ea91b24aecdd5ade583d924a7fbae5
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-18 18:14:30 +0300

    style: change padding of nav

--- markdown-editor/frontend/src/components/nav.tsx
+++ markdown-editor/frontend/src/components/nav.tsx
@@ -6,7 +6,7 @@ import SearchBar from './search-bar';
 const Nav = () => {
   const { session } = useGetSession();
   return (
-    <div className='w-full flex items-center justify-between p-4'>
+    <div className='w-full flex items-center justify-between px-2 py-4'>
       {!session && <SignInButton />}
       {session && <SearchBar />}
       <ModeToggle />
