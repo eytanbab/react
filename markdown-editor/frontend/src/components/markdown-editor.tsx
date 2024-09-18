189209f55714fae0a2d7898d4904000f18ad0d76
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-19 01:23:04 +0300

    feat: replace placeholder markdown with functional implementation

--- markdown-editor/frontend/src/components/markdown-editor.tsx
+++ markdown-editor/frontend/src/components/markdown-editor.tsx
@@ -1,14 +1,20 @@
 import { useState } from 'react';
 import Content from './content';
-import Markdown from './markdown';
+import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 const MarkdownEditor = () => {
   const [markdown, setMarkdown] = useState<string>('');
 
   return (
-    <div className='grid grid-cols-2 h-[calc(100%-4rem)] gap-4 px-2 py-8 w-full'>
+    <div className='grid grid-cols-2 h-[calc(100%-4rem)] gap-4 p-2 w-full'>
       <Content setMarkdown={setMarkdown} />
-      <Markdown markdown={markdown} />
+      <Markdown
+        remarkPlugins={[remarkGfm]}
+        className='markdown w-full bg-slate-50 drop-shadow-md dark:bg-slate-900 p-4 rounded-xl break-words max-h-full overflow-auto'
+      >
+        {markdown}
+      </Markdown>
     </div>
   );
 };
