a1ac8a32f3c9579571eb8f637c154592c391036a
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-19 01:10:55 +0300

    style: remove flex

--- markdown-editor/frontend/src/components/markdown.tsx
+++ markdown-editor/frontend/src/components/markdown.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 const Markdown = ({ markdown }: Props) => {
   return (
-    <div className='flex-1 shrink-0 bg-slate-50 drop-shadow-md dark:bg-slate-900 p-4 rounded-xl break-words max-h-full overflow-auto'>
+    <div className=' w-full bg-slate-50 drop-shadow-md dark:bg-slate-900 p-4 rounded-xl break-words max-h-full overflow-auto '>
       {markdown}
     </div>
   );
