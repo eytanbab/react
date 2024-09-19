ec7389e1d4edcbd2de3685e1a2e0164e553dcf07
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-20 02:51:59 +0300

    feat: wrap app component with router

--- markdown-editor/frontend/src/App.tsx
+++ markdown-editor/frontend/src/App.tsx
@@ -4,23 +4,36 @@ import Separator from './components/separator';
 import Nav from './components/nav';
 import MarkdownEditor from './components/markdown-editor';
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   const [refresh, setRefresh] = useState(false);
+
   const handleSave = () => {
     setRefresh((prev) => !prev); // Toggle to trigger re-render
   };
   return (
-    <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
-      <div className='h-screen flex bg-slate-100 text-slate-950 dark:bg-slate-950 dark:text-slate-100'>
-        <SideNav refresh={refresh} />
-        <Separator />
-        <div className='flex flex-col w-full gap-x-2'>
-          <Nav />
-          <MarkdownEditor onSave={handleSave} />
+    <Router>
+      <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
+        <div className='h-screen flex bg-slate-100 text-slate-950 dark:bg-slate-950 dark:text-slate-100'>
+          <SideNav refresh={refresh} />
+          <Separator />
+          <div className='flex flex-col w-full gap-x-2'>
+            <Nav />
+            <Routes>
+              <Route
+                path='/'
+                element={<MarkdownEditor onSave={handleSave} />}
+              />
+              <Route
+                path='/:id'
+                element={<MarkdownEditor onSave={handleSave} />}
+              />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </ThemeProvider>
+      </ThemeProvider>
+    </Router>
   );
 };
 
