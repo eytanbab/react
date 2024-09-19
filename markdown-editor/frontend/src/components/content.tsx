2c9aa2804dd9bac1149a2264a99f55f6916729d2
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-19 16:46:13 +0300

    refactor: add save button and update props to include markdown

--- markdown-editor/frontend/src/components/content.tsx
+++ markdown-editor/frontend/src/components/content.tsx
@@ -1,17 +1,23 @@
+import SaveButton from './save-button';
+
 type Props = {
+  markdown: string;
   setMarkdown: (e: string) => void;
 };
 
-const Content = ({ setMarkdown }: Props) => {
+const Content = ({ markdown, setMarkdown }: Props) => {
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setMarkdown(e.target.value);
   };
 
   return (
-    <textarea
-      onChange={handleChange}
-      className='w-full border-none outline-none resize-none bg-slate-50 dark:bg-slate-900 p-4 rounded-xl drop-shadow-md'
-    />
+    <div className='w-full border-none outline-none resize-none bg-slate-50 dark:bg-slate-900 p-4 rounded-xl drop-shadow-md relative'>
+      <textarea
+        onChange={handleChange}
+        className='w-full h-full border-none outline-none resize-none bg-slate-50 dark:bg-slate-900'
+      />
+      <SaveButton markdown={markdown} />
+    </div>
   );
 };
 
