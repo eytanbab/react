84e3b42259a2c77bfb7baa716228d99dd4cf8f1e
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-20 02:47:29 +0300

    feat: add fetch markdown by id hook

---
+++ markdown-editor/frontend/src/hooks/use-get-markdown.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import { supabase } from '../../lib/supabaseClient';
+
+export const useGetMarkdown = (id: string | undefined) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [markdown, setMarkdown] = useState('');
+
+  useEffect(() => {
+    const fetchMarkdown = async () => {
+      if (!id) return;
+      setLoading(true);
+      try {
+        const { data, error } = await supabase
+          .from('markdown')
+          .select('content')
+          .eq('id', id)
+          .single();
+        if (error) throw error;
+        setMarkdown(data.content);
+        console.log(data.content);
+      } catch (err) {
+        setError('Failed to fetch markdown');
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchMarkdown();
+  }, [id]);
+
+  return { markdown, loading, error };
+};
