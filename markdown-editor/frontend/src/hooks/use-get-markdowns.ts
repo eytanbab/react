e2546b5d9abc1387e23a5626fc029e33e871a8e1
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-19 16:44:51 +0300

    feat: create hook to fetch saved markdowns

---
+++ markdown-editor/frontend/src/hooks/use-get-markdowns.ts
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import { supabase } from '../../lib/supabaseClient';
+import { useGetSession } from './use-get-session';
+
+type Markdown = {
+  id: string;
+  title?: string;
+  content: string;
+  is_favorite: boolean;
+  user_id: string;
+};
+
+export const useGetMarkdowns = () => {
+  const [markdowns, setMarkdowns] = useState<Markdown[] | null>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const { session } = useGetSession();
+  const userId = session?.user.id;
+
+  useEffect(() => {
+    if (!userId) return;
+    const getMarkdowns = async () => {
+      setLoading(true);
+      try {
+        const { data: markdown, error } = await supabase
+          .from('markdown')
+          .select('id,title,content,is_favorite,user_id')
+          .eq('user_id', userId);
+
+        if (error) {
+          throw error;
+        }
+
+        setMarkdowns(markdown);
+      } catch (err) {
+        setError('Failed to fetch saved markdowns');
+        console.error('Error fetching markdowns:', err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getMarkdowns();
+  }, [userId]);
+
+  return { markdowns, loading, error };
+};
