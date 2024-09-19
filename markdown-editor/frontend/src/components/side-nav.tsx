76ff57205a345aa30a43a1bdbfb122152aa2a03f
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-19 16:47:16 +0300

    feat: add saved markdown cards in side nav

--- markdown-editor/frontend/src/components/side-nav.tsx
+++ markdown-editor/frontend/src/components/side-nav.tsx
@@ -1,12 +1,24 @@
+import { useGetMarkdowns } from '../hooks/use-get-markdowns';
 import { useGetSession } from '../hooks/use-get-session';
+import SavedMarkdownCard from './saved-markdown-card';
 import UserAvatar from './user-avatar';
 
 const SideNav = () => {
   const { session } = useGetSession();
+  const { markdowns, loading, error } = useGetMarkdowns();
+
+  if (!session) return null;
 
   return (
-    <div className='h-full w-80 p-4 flex items-start shrink-0'>
-      {session && <UserAvatar />}
+    <div className='h-full w-80 p-4 flex flex-col items-start shrink-0 gap-8'>
+      <UserAvatar />
+      <div className='w-full flex flex-col gap-2 overflow-y-auto h-full'>
+        {loading && <div>Loading...</div>}
+        {error && <div>Error: {error}</div>}
+        {markdowns?.map((markdown) => {
+          return <SavedMarkdownCard key={markdown.id} markdown={markdown} />;
+        })}
+      </div>
     </div>
   );
 };
