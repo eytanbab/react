b3dfb07700093fb6f8ac3b4c69b22372db1f462e
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-21 02:39:24 +0300

    fix: correct route and button text toggling

--- markdown-editor/frontend/src/components/nav.tsx
+++ markdown-editor/frontend/src/components/nav.tsx
@@ -14,9 +14,11 @@ const Nav = () => {
       <div className='flex gap-4 items-center'>
         <Link
           className='border border-slate-800/25 dark:border-slate-300/50 rounded-lg py-1 px-2'
-          to={location.pathname === '/' ? 'how-to-use' : '/'}
+          to={location.pathname === '/how-to-use' ? '/' : 'how-to-use'}
         >
-          {location.pathname === '/' ? 'How to use' : 'Markdown Editor'}
+          {location.pathname === '/how-to-use'
+            ? 'Markdown Editor'
+            : 'How to use'}
         </Link>
         <ModeToggle />
       </div>
