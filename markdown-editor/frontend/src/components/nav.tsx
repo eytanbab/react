1c4113a47317d8ff940dcf475373a6bf73ea2dfa
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-20 17:53:16 +0300

    feat: add link to markdown guide component

--- markdown-editor/frontend/src/components/nav.tsx
+++ markdown-editor/frontend/src/components/nav.tsx
@@ -2,6 +2,7 @@ import { useGetSession } from '../hooks/use-get-session';
 import SignInButton from './sign-in-button';
 import ModeToggle from './mode-toggle';
 import SearchBar from './search-bar';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
   const { session } = useGetSession();
@@ -9,7 +10,15 @@ const Nav = () => {
     <div className='w-full flex items-center justify-between px-2 py-4'>
       {!session && <SignInButton />}
       {session && <SearchBar />}
-      <ModeToggle />
+      <div className='flex gap-4 items-center'>
+        <Link
+          className='border border-slate-800/25 dark:border-slate-300/50 rounded-lg py-1 px-2'
+          to='how-to-use'
+        >
+          How to use
+        </Link>
+        <ModeToggle />
+      </div>
     </div>
   );
 };
