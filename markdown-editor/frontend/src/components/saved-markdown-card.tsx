8dcdd91e30f430c63a8ef6a8fc8d4b48804c4285
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-20 15:00:18 +0300

    style: highlight the selected saved markdown card

--- markdown-editor/frontend/src/components/saved-markdown-card.tsx
+++ markdown-editor/frontend/src/components/saved-markdown-card.tsx
@@ -8,15 +8,24 @@ type Props = {
     is_favorite: boolean;
     user_id: string;
   };
+  isActive: boolean;
 };
 
 const handleDelete = (e: React.MouseEvent<HTMLButtonElement>) => {
   e.preventDefault();
 };
 
-const SavedMarkdownCard = ({ markdown }: Props) => {
+const SavedMarkdownCard = ({ markdown, isActive }: Props) => {
   return (
-    <div className='relative bg-slate-50 dark:bg-slate-900 w-full min-h-24 rounded-xl drop-shadow-md p-2'>
+    <div
+      className={`
+    ${
+      isActive
+        ? 'text-slate-800 dark:text-slate-200'
+        : 'text-slate-400 dark:text-slate-600'
+    }
+    relative bg-slate-50 dark:bg-slate-900 w-full min-h-24 rounded-xl drop-shadow-md p-2 `}
+    >
       <div className='line-clamp-3'>{markdown.content}</div>
       <button onClick={handleDelete} className='absolute top-2 right-2 z-50'>
         <IoIosCloseCircleOutline className='size-6' />
