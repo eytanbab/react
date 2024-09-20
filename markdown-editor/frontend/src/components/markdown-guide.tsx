080ef6d534fc108c2dde44a05ee71955572bd372
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-20 17:53:03 +0300

    feat: add markdown guide compoent

---
+++ markdown-editor/frontend/src/components/markdown-guide.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MarkdownGuide = () => {
+  return (
+    <div className='h-[calc(100%-4rem)] gap-4 p-2 w-full'>MarkdownGuide</div>
+  );
+};
+
+export default MarkdownGuide;
