8696e33026d92d1b36239e0b45a3e7c0621d85e8
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-20 03:00:07 +0300

    feat: add delete button for saved markdown

--- markdown-editor/frontend/src/components/saved-markdown-card.tsx
+++ markdown-editor/frontend/src/components/saved-markdown-card.tsx
@@ -10,11 +10,17 @@ type Props = {
   };
 };
 
+const handleDelete = (e: React.MouseEvent<HTMLButtonElement>) => {
+  e.preventDefault();
+};
+
 const SavedMarkdownCard = ({ markdown }: Props) => {
   return (
     <div className='relative bg-slate-50 dark:bg-slate-900 w-full min-h-24 rounded-xl drop-shadow-md p-2'>
       <div className='line-clamp-3'>{markdown.content}</div>
-      <IoIosCloseCircleOutline className='size-6 absolute top-2 right-2' />
+      <button onClick={handleDelete} className='absolute top-2 right-2 z-50'>
+        <IoIosCloseCircleOutline className='size-6' />
+      </button>
     </div>
   );
 };
