ab1fd124a2f1fa29ebb245a2bdc8624ffe6eaaf6
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-21 00:59:07 +0300

    feat: add location pathname to navigate between markdown editor and how to use page

--- markdown-editor/frontend/src/components/nav.tsx
+++ markdown-editor/frontend/src/components/nav.tsx
@@ -2,10 +2,11 @@ import { useGetSession } from '../hooks/use-get-session';
 import SignInButton from './sign-in-button';
 import ModeToggle from './mode-toggle';
 import SearchBar from './search-bar';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Nav = () => {
   const { session } = useGetSession();
+  const location = useLocation();
   return (
     <div className='w-full flex items-center justify-between px-2 py-4'>
       {!session && <SignInButton />}
@@ -13,9 +14,9 @@ const Nav = () => {
       <div className='flex gap-4 items-center'>
         <Link
           className='border border-slate-800/25 dark:border-slate-300/50 rounded-lg py-1 px-2'
-          to='how-to-use'
+          to={location.pathname === '/' ? 'how-to-use' : '/'}
         >
-          How to use
+          {location.pathname === '/' ? 'How to use' : 'Markdown Editor'}
         </Link>
         <ModeToggle />
       </div>
