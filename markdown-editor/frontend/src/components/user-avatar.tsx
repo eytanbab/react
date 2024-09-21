fabe2c32c9ac61a011028778ddd25df43e6f42f5
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-21 03:21:06 +0300

    chore: remove placeholder logout

--- markdown-editor/frontend/src/components/user-avatar.tsx
+++ markdown-editor/frontend/src/components/user-avatar.tsx
@@ -1,29 +1,19 @@
 import { Link } from 'react-router-dom';
-import { supabase } from '../../lib/supabaseClient';
 import { useGetSession } from '../hooks/use-get-session';
 
 const UserAvatar = () => {
   const { session } = useGetSession();
-
-  const handleLogout = async () => {
-    const { error } = await supabase.auth.signOut();
-    if (error) console.error('Error logging out:', error.message);
-    else {
-      console.log('Logged out successfully');
-    }
-  };
 
   return session ? (
     <div className='w-full flex items-center justify-between'>
       <Link to='/' className='font-bold text-lg'>
         Markdown Editor
       </Link>
-      <button onClick={handleLogout}>
-        <img
-          src={session.user.user_metadata.picture}
-          className='size-8 rounded-full'
-        />
-      </button>
+
+      <img
+        src={session.user.user_metadata.picture}
+        className='size-8 rounded-full'
+      />
     </div>
   ) : null;
 };
