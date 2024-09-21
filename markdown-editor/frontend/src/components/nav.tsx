be00e577e9f806b158caf4fd79ddfd0d9d6168eb
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-21 14:31:59 +0300

    refactor: remove search bar from nav

--- markdown-editor/frontend/src/components/nav.tsx
+++ markdown-editor/frontend/src/components/nav.tsx
@@ -1,7 +1,6 @@
 import { useGetSession } from '../hooks/use-get-session';
 import SignInButton from './sign-in-button';
 import ModeToggle from './mode-toggle';
-import SearchBar from './search-bar';
 import { Link, useLocation } from 'react-router-dom';
 
 const Nav = () => {
@@ -10,8 +9,8 @@ const Nav = () => {
   return (
     <div className='w-full flex items-center justify-between px-2 py-4'>
       {!session && <SignInButton />}
-      {session && <SearchBar />}
-      <div className='flex gap-4 items-center'>
+      {/* {session && <SearchBar />} */}
+      <div className='flex gap-4 items-center w-full justify-end'>
         <Link
           className='border border-slate-800/25 dark:border-slate-300/50 rounded-lg py-1 px-2'
           to={location.pathname === '/how-to-use' ? '/' : 'how-to-use'}
