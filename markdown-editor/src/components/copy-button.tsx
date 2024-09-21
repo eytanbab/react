446b8b83447e7736a77fcdc9b613ffc9ef671a87
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-21 17:06:17 +0300

    fix: disable copy button when content is empty

--- markdown-editor/src/components/copy-button.tsx
+++ markdown-editor/src/components/copy-button.tsx
@@ -3,11 +3,14 @@ type Props = {
 };
 
 const CopyButton = ({ content }: Props) => {
+  const disabled = content === '';
+
   const handleCopy = () => {
     navigator.clipboard.writeText(content);
   };
   return (
     <button
+      disabled={disabled}
       onClick={handleCopy}
       className='bg-slate-900 dark:bg-slate-100 text-slate-100 dark:text-slate-900 absolute px-4 py-1 rounded-full right-4 bottom-6 drop-shadow-md disabled:bg-slate-400 disabled:dark:bg-slate-700 disabled:drop-shadow-none'
     >
