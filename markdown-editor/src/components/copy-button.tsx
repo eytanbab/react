f20427aed8824854e5322debd549373309225f1e
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-21 17:05:16 +0300

    feat: add copy button

---
+++ markdown-editor/src/components/copy-button.tsx
@@ -0,0 +1,19 @@
+type Props = {
+  content: string;
+};
+
+const CopyButton = ({ content }: Props) => {
+  const handleCopy = () => {
+    navigator.clipboard.writeText(content);
+  };
+  return (
+    <button
+      onClick={handleCopy}
+      className='bg-slate-900 dark:bg-slate-100 text-slate-100 dark:text-slate-900 absolute px-4 py-1 rounded-full right-4 bottom-6 drop-shadow-md disabled:bg-slate-400 disabled:dark:bg-slate-700 disabled:drop-shadow-none'
+    >
+      Copy
+    </button>
+  );
+};
+
+export default CopyButton;
