1fa8727e1a235925c2d64e9aab9987253b32db68
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-22 22:57:51 +0300

    feat: add responsiveness to nav

--- markdown-editor/src/components/nav.tsx
+++ markdown-editor/src/components/nav.tsx
@@ -1,16 +1,30 @@
 import { useGetSession } from '../hooks/use-get-session';
 import SignInButton from './sign-in-button';
 import ModeToggle from './mode-toggle';
+import Title from './title';
+import LogoutButton from './logout-button';
+import { BiMenu } from 'react-icons/bi';
 // import { Link, useLocation } from 'react-router-dom';
 
 const Nav = () => {
   const { session } = useGetSession();
   // const location = useLocation();
   return (
-    <div className='w-full flex items-center justify-between px-2 py-4'>
+    <div className='w-full flex items-center justify-between p-4'>
       {!session && <SignInButton />}
       {/* {session && <SearchBar />} */}
       <div className='flex gap-4 items-center w-full justify-end'>
+        <div className='flex w-full items-center gap-2 lg:hidden'>
+          <BiMenu className='size-6' />
+          <Title />
+        </div>
+        <div className='lg:hidden'>
+          <LogoutButton />
+        </div>
+        <div>
+          <ModeToggle />
+        </div>
+
         {/* <Link
           className='border border-slate-800/25 dark:border-slate-300/50 rounded-lg py-1 px-2'
           to={location.pathname === '/how-to-use' ? '/' : 'how-to-use'}
@@ -19,7 +33,6 @@ const Nav = () => {
             ? 'Markdown Editor'
             : 'How to use'}
         </Link> */}
-        <ModeToggle />
       </div>
     </div>
   );
