8f25ab72cf69891715926f9c38729efb9b1a48f9
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-23 00:12:06 +0300

    refactor: remove authentication from title

--- markdown-editor/src/components/title.tsx
+++ markdown-editor/src/components/title.tsx
@@ -1,14 +1,11 @@
 import { Link } from 'react-router-dom';
-import { useGetSession } from '../hooks/use-get-session';
 
 const Title = () => {
-  const { session } = useGetSession();
-
-  return session ? (
-    <Link to='/' className='font-bold text-lg w-full'>
+  return (
+    <Link to='/' className='font-bold text-lg'>
       Markdown Editor
     </Link>
-  ) : null;
+  );
 };
 
 export default Title;
