e4009b463026c55c8e68b7dbf0b0c9757fbc080b
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-22 22:59:47 +0300

    refactor: import markdown viewer component

--- markdown-editor/src/components/markdown-editor.tsx
+++ markdown-editor/src/components/markdown-editor.tsx
@@ -1,10 +1,8 @@
 import { useEffect } from 'react';
 import Content from './content';
-import Markdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-import SaveButton from './save-button';
 import { useGetMarkdown } from '../hooks/use-get-markdown';
 import { useLocation, useParams } from 'react-router-dom';
+import MarkdownViewer from './markdown-viewer';
 
 type Props = {
   onSave: () => void;
@@ -26,15 +24,9 @@ const MarkdownEditor = ({ onSave, content, setContent }: Props) => {
   }, [location.pathname, fetchedMarkdown, setContent]);
 
   return (
-    <div className='grid grid-cols-2 h-[calc(100%-4rem)] gap-4 p-2 w-full'>
+    <div className='flex flex-col lg:grid grid-cols-2 gap-4 p-2 w-full h-[calc(100%-4rem)] items-start'>
       <Content markdown={content} setMarkdown={setContent} />
-      <Markdown
-        remarkPlugins={[remarkGfm]}
-        className='markdown w-full p-4 bg-slate-50 drop-shadow-md dark:bg-slate-900 rounded-xl break-words max-h-full overflow-auto whitespace-pre-wrap scrollbar-hide'
-      >
-        {content}
-      </Markdown>
-      <SaveButton content={content} onSave={onSave} />
+      <MarkdownViewer content={content} onSave={onSave} />
     </div>
   );
 };
