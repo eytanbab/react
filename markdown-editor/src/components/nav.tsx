5d0427304ad70cad3ba57dd2a2e869d4a73a7da5
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-23 00:12:39 +0300

    refactor: remove menu icon from nav component

--- markdown-editor/src/components/nav.tsx
+++ markdown-editor/src/components/nav.tsx
@@ -3,19 +3,17 @@ import SignInButton from './sign-in-button';
 import ModeToggle from './mode-toggle';
 import Title from './title';
 import LogoutButton from './logout-button';
-import { BiMenu } from 'react-icons/bi';
 // import { Link, useLocation } from 'react-router-dom';
 
 const Nav = () => {
   const { session } = useGetSession();
   // const location = useLocation();
   return (
-    <div className='w-full flex items-center justify-between p-4'>
+    <div className='w-full hidden lg:flex items-center justify-between p-4'>
       {!session && <SignInButton />}
       {/* {session && <SearchBar />} */}
       <div className='flex gap-4 items-center w-full justify-end'>
         <div className='flex w-full items-center gap-2 lg:hidden'>
-          <BiMenu className='size-6' />
           <Title />
         </div>
         <div className='lg:hidden'>
