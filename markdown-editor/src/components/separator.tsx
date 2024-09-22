852a3b7648b36027f9776722d76a9bf7ca0bbc46
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-22 22:56:22 +0300

    style: update separator component to be hidden on smaller devices

--- markdown-editor/src/components/separator.tsx
+++ markdown-editor/src/components/separator.tsx
@@ -1,6 +1,6 @@
 const Separator = () => {
   return (
-    <div className='h-full w-[2px] bg-slate-950/10 dark:bg-slate-200/10 flex-none' />
+    <div className='h-full w-[2px] bg-slate-950/10 dark:bg-slate-200/10 flex-none hidden lg:block' />
   );
 };
 
